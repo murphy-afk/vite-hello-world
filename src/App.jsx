7a48f1d3dada12c3ac5baddeaa46cbdc28c1c9b9
murphy-afk <bardarenoemi@gmail.com>
2025-11-24 17:00:49 +0100

    feat: button counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import './App.css'
 
 // const App = () => <h1>Hello world</h1>;
+const Greet = ({ name }) => <h1>Hello {name}</h1>;
 
 const Paragraph = () => <p>This is a paragraph</p>;
 
 const Button = () => {
+  let count = 1;
   const onClick = () => {
-    console.log('It works');
+    if (count > 10) {
+      console.log("STOP CLICKING THE BUTTON!!!!");
+    }
+    else {
+      console.log("Button works", count);
+      count++;
+    }
   }
   return (
     <button onClick={onClick}>This is a button</button>
   )
 };
-
-const Greet = ({ name }) => <h1>Hello {name}</h1>;
-
 
 export { Paragraph, Button, Greet }
